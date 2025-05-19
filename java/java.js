8b694d57af20e51f2b115d136d519ec06d702119
darshdeep15 <darsh1508@icloud.com>
2025-05-20 02:16:56 +0530

    port

---
+++ java/java.js
@@ -0,0 +1,9 @@
+// Declaring student information variables
+let studentName = "Darshdeep";
+const studentId = "220bdes014";
+var studentContact = "9811810094";
+
+// Displaying the values in the console
+console.log("Student Name:", studentName);
+console.log("Student ID:", studentId);
+console.log("Student Contact:", studentContact);
